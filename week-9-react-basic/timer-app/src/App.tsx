4932c1e00869a7daae1019702ee9f84c513ade28
Tamanna Sharma <ts.harma2882006@gmail.com>
2025-04-01 20:18:40 +0530

    timer done

--- week-9-react-basic/timer-app/src/App.tsx
+++ week-9-react-basic/timer-app/src/App.tsx
@@ -1,9 +1,51 @@
+import {useRef, useState, useEffect} from "react";
 import Timer from "./Timer"
 
 function App() {
-  // const [min, setMin] = 
-  return (<div className="h-screen w-full flex items-center justify-center">
-    <Timer />
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(10);
+  const minRef = useRef<HTMLInputElement>(null);
+  const secRef = useRef<HTMLInputElement>(null);
+
+  function handleNewTime(){
+    if(!minRef.current || !secRef.current) return;
+
+    //if input left empty it will be 0 as converting empty string to number, well good for us
+    const newMin = Number(minRef.current.value);
+    const newSec = Number(secRef.current.value);
+
+
+    if(newMin < 0 || newSec < 0){
+      alert("Time should be positive");
+      return;
+    }
+    if(newSec > 60){
+      alert("Seconds could maximize to 60");
+      return;
+    }
+    if(newMin > 60){
+      alert("Minutes could maximize to 60");
+      return;
+    }
+
+    setMin(newMin);
+    setSec(newSec);
+    minRef.current.value = "";
+    secRef.current.value = "";
+  }
+
+  //just to check if the values are updating
+  useEffect(() => {
+    console.log(min, sec);
+  }, [min, sec])
+
+  return (<div className="h-screen w-full flex flex-col items-center justify-center">
+    <Timer min={min} sec={sec}/>
+    <div className="flex flex-col items-center space-y-3 border border-gray-400 p-5 rounded-lg mt-5">
+      <input ref={minRef} placeholder="enter new min" type="number" className="border border-gray-300 p-2 rounded-lg"/>
+      <input ref={secRef} placeholder="enter new sec" type="number" className="border border-gray-300 p-2 rounded-lg"/>
+      <button onClick={handleNewTime} className="bg-blue-400 text-white py-1 px-4 rounded-lg cursor-pointer hover:bg-blue-500">Update Timer</button>
+    </div>
   </div>)
 }
 
--- week-9-react-basic/timer-app/src/Timer.tsx
+++ week-9-react-basic/timer-app/src/Timer.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
 
-function Timer() {
-	const [totalSeconds, setTotalSeconds] = useState(10 + 0 * 60);
+function Timer({min, sec}: {min: number, sec: number}) {
+	const [totalSeconds, setTotalSeconds] = useState(sec + (min * 60));
 	const [isRunning, setIsRunning] = useState(false);
 
-	const min = Math.floor(totalSeconds / 60);
-	const sec = totalSeconds % 60;
+	const localMin = Math.floor(totalSeconds / 60);
+	const localSec = totalSeconds % 60;
   
+	//this will run if min and sec updated (by inputs from app.tsx)
+	useEffect(() => {
+		setTotalSeconds(sec + (min * 60));
+	}, [min, sec]);
 
 	useEffect(() => {
 		if (!isRunning) return;
 		const timer = setInterval(() => {
 			console.log("interval is running");
 
-			setTotalSeconds((totalSeconds) => {
-				if (totalSeconds === 0) {
+			setTotalSeconds((prevTotalSeconds) => {
+				if (prevTotalSeconds === 0) {
 					clearInterval(timer);
+					setIsRunning(false);
 					return 0;
 				}
-				return totalSeconds - 1;
+				return prevTotalSeconds - 1;
 			});
 		}, 1000);
 		return () => {
@@ -28,9 +33,9 @@ function Timer() {
 	}, [isRunning]);
 
 	return (
-		<div className="text-5xl">
-			{isFineTimeUnit(min) ? min : `0${min}`} :{" "}
-			{isFineTimeUnit(sec) ? sec : `0${sec}`}
+		<div className="text-5xl flex flex-col items-center ">
+			<h1>{isFineTimeUnit(localMin) ? localMin : `0${localMin}`} :  
+			{isFineTimeUnit(localSec) ? localSec : `0${localSec}`}</h1>
 			<div className="text-3xl flex justify-center items-center p-5 space-x-3">
 				{totalSeconds > 0 ? (
 					<button
@@ -42,6 +47,7 @@ function Timer() {
 				) : (
 					<h1>Done!</h1>
 				)}
+				<button onClick={()=>setTotalSeconds(sec + (min * 60))} className="bg-blue-400 text-white py-1 px-4 rounded-lg cursor-pointer hover:bg-blue-500">Reset</button>
 			</div>
 		</div>
 	);
