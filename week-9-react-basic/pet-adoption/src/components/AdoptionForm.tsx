c5c36e33fd3b9e9936d044537818d4175ba2d256
Tamanna Sharma <ts.harma2882006@gmail.com>
2025-04-07 18:37:50 +0530

    added validation

--- week-9-react-basic/pet-adoption/src/components/AdoptionForm.tsx
+++ week-9-react-basic/pet-adoption/src/components/AdoptionForm.tsx
@@ -1,19 +1,39 @@
-import {FormEvent, useRef} from "react";
+import {FormEvent, useRef, useState} from "react";
 import Button from "./Button";
 import Input from "./Input";
 
 
 function AdoptionForm(){
 
-  const FormRef = useRef(null);
+  const FormRef = useRef<HTMLFormElement>(null);
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
 
   function handleSubmit(e: FormEvent<HTMLFormElement>){
     e.preventDefault();
-    console.log(e.target);
     
     const formDataIterable = new FormData(FormRef.current!).entries();
     const formData = Object.fromEntries(formDataIterable);
     console.log(formData);
+
+    const newErrors: Record<string, string> = {};
+    //name and email validations are already there, just added required and used type email, also select are also required, so we need to add validation only for phone no.
+    if( (formData["phone"] as string).length < 10 || (formData["phone"] as string).length > 10 || !/^\d+$/.test(formData["phone"] as string) ){
+      newErrors["phone"] = "Phone number must be 10 digits and contain only numbers";
+    }
+    
+    //object.keys se hume basically array mil jaega to isse hoga kya ki length pta chl jaegi na ki koi bhi error h ya nhi
+    if(Object.keys(newErrors).length > 0){
+      setErrors(newErrors);
+      console.log(newErrors);
+      return;
+    }
+
+    console.log("Form submitted successfully", formData);
+    setErrors({});
+    // Reset the form after successful submission
+    FormRef.current?.reset();
+
   }
 
   return <div className="bg-glass p-5 rounded-lg mt-5">
@@ -24,7 +44,7 @@ function AdoptionForm(){
       <Input label="Breed" type="select" placeholder="Select Breed" options={["breed1", "breed2"]}/>
       <Input label="Your Name" type="text" placeholder="Your Name" />
       <Input label="Email" type="email" placeholder="Email" />
-      <Input label="Phone" type="phone" placeholder="Phone" />
+      <Input label="Phone" type="phone" placeholder="Phone" error={errors["phone"]}/>
       <div className="flex justify-between items-center gap-4 ">
       <Button text="Reset" type="reset" className="grow"varient="secondary"/>
       <Button text="Submit" type="submit" className="grow"/>
--- week-9-react-basic/pet-adoption/src/components/Input.tsx
+++ week-9-react-basic/pet-adoption/src/components/Input.tsx
@@ -3,17 +3,18 @@ type InputProps = {
   placeholder: string;
   label: string;
   options: string[];
+  error: string;
 }
 
 const inputStyles = "bg-white p-2 rounded-lg w-60";
 const divStyles = "flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-10 mb-2";
 
-function Input({type="text", placeholder, label, options}: Partial<InputProps>){
+function Input({type="text", placeholder, label, options, error}: Partial<InputProps>){
   const inputName = label?.split(" ").join("-").toLowerCase();
   return <div className={divStyles}>
     <label htmlFor={inputName} className={"text-primary-black"}>{label}</label>
-    { type !== "select" ? <input name={inputName} placeholder={placeholder} type={type} id={inputName} className={inputStyles}/> : 
-      <select name={inputName} id={inputName} className={inputStyles}>
+    { type !== "select" ? <div><input required name={inputName} placeholder={placeholder} type={type} id={inputName} className={inputStyles}/> {error && <p className="text-primary-black text-sm max-w-60">{error}</p>}</div> : 
+      <select required name={inputName} id={inputName} className={inputStyles}>
         <option value="" className="text-gray-400">{placeholder}</option>
         {options && options.map((option, i)=> <option value={option} key={`${option + i}`}>{option}</option>)}
       </select>
